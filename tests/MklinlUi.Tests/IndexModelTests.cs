87a97017e4c8b5f397bcd58e872ef2bca51ede65
Courtland <45576496+Courtland9777@users.noreply.github.com>
2025-08-11 23:59:20 -0400

    Test symlink filename validation

---
+++ tests/MklinlUi.Tests/IndexModelTests.cs
@@ -0,0 +1,52 @@
+using System.IO;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using MklinlUi.Core;
+using MklinlUi.Fakes;
+using MklinlUi.WebUI.Pages;
+using Xunit;
+
+namespace MklinlUi.Tests;
+
+public class IndexModelTests
+{
+    [Fact]
+    public async Task OnPostAsync_returns_error_for_invalid_filename()
+    {
+        var devService = new FakeDeveloperModeService();
+        var manager = new SymlinkManager(devService, new FakeSymlinkService());
+        var model = new IndexModel(manager, devService)
+        {
+            DestinationFolder = "/dest",
+            SourceFiles = [CreateFormFile("")]
+        };
+
+        var result = await model.OnPostAsync();
+
+        model.Success.Should().BeFalse();
+        model.Message.Should().Be("One or more file names are invalid.");
+    }
+
+    [Fact]
+    public async Task OnPostAsync_returns_error_when_source_missing()
+    {
+        var devService = new FakeDeveloperModeService();
+        var manager = new SymlinkManager(devService, new FakeSymlinkService());
+        var model = new IndexModel(manager, devService)
+        {
+            DestinationFolder = "/dest",
+            SourceFiles = [CreateFormFile("missing.txt")]
+        };
+
+        var result = await model.OnPostAsync();
+
+        model.Success.Should().BeFalse();
+        model.Message.Should().Contain("Source file not found");
+    }
+
+    private static IFormFile CreateFormFile(string fileName)
+    {
+        var stream = new MemoryStream();
+        return new FormFile(stream, 0, 0, fileName, fileName);
+    }
+}
