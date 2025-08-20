845ca494b975b0d2769bf17e76c483950ab585b1
Courtland <45576496+Courtland9777@users.noreply.github.com>
2025-08-20 04:47:30 -0400

    Handle directories in dropFolders

--- tests/client/dropFolders.test.js
+++ tests/client/dropFolders.test.js
@@ -6,9 +6,22 @@ const target = { value: '', classList: { remove: () => {} } };
 
 global.document = { getElementById: () => target };
 
+// Existing FileList support
 dropFolders({ preventDefault: () => { prevented = true; }, dataTransfer: { files: [{ path: 'C:/new' }] } });
-
 assert.strictEqual(target.value, 'C:/new');
+assert.strictEqual(prevented, true);
+
+// Reset for DataTransferItemList scenario
+prevented = false;
+target.value = '';
+
+const items = [{
+    webkitGetAsEntry: () => ({ isDirectory: true, fullPath: '/C/dir' })
+}];
+
+dropFolders({ preventDefault: () => { prevented = true; }, dataTransfer: { files: [], items } });
+
+assert.strictEqual(target.value, 'C/dir');
 assert.strictEqual(prevented, true);
 
 console.log('dropFolders test passed');
